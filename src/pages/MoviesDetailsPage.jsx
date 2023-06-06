5061bf9af3b6eca43516f1d73e706980d63865e1
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-06-06 03:07:46 +0300

    Update MoviesDetailsPage.jsx

--- src/pages/MoviesDetailsPage.jsx
+++ src/pages/MoviesDetailsPage.jsx
@@ -1,23 +1,23 @@
 import { useParams } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import MoviesDetails from 'components/MovieDetails/MovieDetails';
-// import getMovieDetails from 'api/getMovieDetails';
+import getMovieDetails from 'api/getMovieDetails';
 
 const MoviesDetailsPage = () => {
   const { movieId } = useParams();
-    // const [movie, setMovie] = useState({});
+    const [movie, setMovie] = useState({});
     
-    // useEffect(() => {
-    //   const responseResolved = movie => {
-    //     setMovie(movie);
-    //   };
+    useEffect(() => {
+      const responseResolved = movie => {
+        setMovie(movie);
+      };
 
-    //   const responseRejected = error => {
-    //     console.log(error.message);
-    //   };
+      const responseRejected = error => {
+        console.log(error.message);
+      };
 
-    //   getMovieDetails(movieId).then(responseResolved).catch(responseRejected);
-    // }, [movieId]);
+      getMovieDetails(movieId).then(responseResolved).catch(responseRejected);
+    }, [movieId]);
 
   return <MoviesDetails id={movieId} />;
 };
